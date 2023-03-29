d9895a7d334499b3d9b26d495ddf385fedd555c8
vladimir <vladimir.business0@gmail.com>
2023-03-29 13:08:59 +0200

    modal functionality

---
+++ assets/js/modal.js
@@ -0,0 +1,28 @@
+// Modal
+const modalWrapper = document.querySelector('.newsletter__popup')
+const modalBackground = document.querySelector('.newsletter__popup__back')
+const modalWindow = document.querySelector('.newsletter__popup__front')
+const closeButton = document.querySelector('.close')
+const body = document.querySelector('body')
+
+window.onload = e => {
+  closeButton.addEventListener('click', () => {
+    closeModal()
+  })
+
+  setTimeout(() => {
+    showModal()
+  }, 1000)
+}
+const showModal = function () {
+  modalWrapper.classList.remove('hidden')
+
+  if (!modalWrapper.classList.contains('hidden')) {
+    body.style.overflow = 'hidden'
+  }
+}
+
+const closeModal = function () {
+  modalWrapper.classList.add('hidden')
+  body.style.overflow = 'auto'
+}
